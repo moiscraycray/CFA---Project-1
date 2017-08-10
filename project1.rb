225eee670b7db7c305fd53352045f62bba50f891
J1marotta <jim_is55@hotmail.com>
2017-08-10 19:10:41 +1000

    debugging 710pm

--- project1.rb
+++ project1.rb
@@ -1,16 +1,16 @@
 =begin
 
-require 'artii'
+ PROJECT 1 Thursday 10th August, Oliva, Carmen, Nathan, James
+ Group project to impress Trent in what ever way possible.
 
-a = Artii::Base.new(:font => 'slant')
-puts a.asciify('Go kill yourself - Louis C.K. 2017')
+ like a good old fashion game of is this a pixelated picture of trent or not?
 
 =end
 
 system("clear")
 
 require 'artii' #This is one of the gems, ASCII text
-require 'catpix' #This gem prints out the pixelated image
+require 'catpix'#This gem prints out the pixelated image
 
 =begin
 The first class is the introduction to the program.
@@ -54,15 +54,21 @@ class Introduction
     # save their input as
     case @difficulty
     when "easy", "e"
-        @difficulty = "easy"
-        break
-      when "medium", "m"
-        @difficulty = "medium"
-      when "hard" , "h"
-        @difficulty = "hard"
+      @difficulty = "easy"
+      randomise
+      break
+    when "medium", "m"
+      @difficulty = "medium"
+      randomise
+      break
+    when "hard" , "h"
+      @difficulty = "hard"
+      randomise
+      break
       else
         "Error, invalid input, please choose Easy, Medium or Hard"
     end
+
 
 
   end
@@ -92,10 +98,9 @@ class Player
     puts "Wrong"
 
   end
-<<<<<<< HEAD
+
 end
-=======
->>>>>>> refs/remotes/origin/master
+
 =begin
     w - Create an empty file for writing.
     a - Append to a file.The file is created if it does not exist.
@@ -119,155 +124,141 @@ class Image_keeper
   #Individual users will need to change the directory of where the images are stored
   def keep_files
     @easy_trent = [
-      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy1small.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy2.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy3.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy4.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy5.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy6.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy7.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy8.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy9.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy10.jpg")
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/easy/trenteasy1small.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/easy/trenteasy2.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/easy/trenteasy3.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/easy/trenteasy4.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/easy/trenteasy5.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/easy/trenteasy6.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/easy/trenteasy7.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/easy/trenteasy8.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/easy/trenteasy9.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/easy/trenteasy10.jpg")
     ]
     @easy_not_trent = [
-      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy1.jpg"),
-      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy2.jpg"),
-      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy3.jpg"),
-      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy4.jpg"),
-      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy5.jpg"),
-      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy6.jpg"),
-      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy7.jpg"),
-      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy8.jpg"),
-      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy9.jpg"),
-      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy10.jpg")
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/not_trent/easy/nteasy1.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/not_trent/easy/nteasy2.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/not_trent/easy/nteasy3.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/not_trent/easy/nteasy4.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/not_trent/easy/nteasy5.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/not_trent/easy/nteasy6.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/not_trent/easy/nteasy7.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/not_trent/easy/nteasy8.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/not_trent/easy/nteasy9.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/not_trent/easy/nteasy10.jpg")
     ]
     @medium_trent = [
-      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed1.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed2.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed3.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed4.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed5.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed6.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed7.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed8.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed9.jpg"),
-      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed10.jpg")
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/medium/trentmed1.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/medium/trentmed2.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/medium/trentmed3.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/medium/trentmed4.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/medium/trentmed5.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/medium/trentmed6.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/medium/trentmed7.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/medium/trentmed8.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/medium/trentmed9.jpg"),
+      open(ENV['HOME']+"/sites/GroupAssignment/project1/trent/medium/trentmed10.jpg")
     ]
     @medium_not_trent = [
-      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed1.jpg"),
-      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed2.jpg"),
-      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed3.jpg"),
-      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed4.jpg"),
-      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed5.jpg"),
-      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed6.jpg"),
-      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed7.jpg"),
-      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed8.jpg"),
-      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed9.jpg"),
-      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed10.jpg")
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/medium/ntmed1.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/medium/ntmed2.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/medium/ntmed3.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/medium/ntmed4.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/medium/ntmed5.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/medium/ntmed6.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/medium/ntmed7.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/medium/ntmed8.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/medium/ntmed9.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/medium/ntmed10.jpg")
     ]
     @hard_trent = [
-      open(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
-      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
-      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
-      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
-      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
-      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
-      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
-      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
-      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
-      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/trent/hard/trenthard1.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/trent/hard/trenthard2.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/trent/hard/trenthard3.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/trent/hard/trenthard4.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/trent/hard/trenthard5.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/trent/hard/trenthard6.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/trent/hard/trenthard7.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/trent/hard/trenthard8.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/trent/hard/trenthard9.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/trent/hard/trenthard10.jpg"),
     ]
-    @hard_not_trent = []
+    @hard_not_trent = [
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/hard/nthard1.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/hard/nthard2.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/hard/nthard3.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/hard/nthard4.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/hard/nthard5.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/hard/nthard6.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/hard/nthard7.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/hard/nthard8.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/hard/nthard9.jpg"),
+      open(ENV['HOME']+"sites/GroupAssignment/project1/not_trent/hard/nthard10.jpg")
+    ]
   end
 
   # method to randomly select the arrays of pictures.
   # variables :  picture they are looking at = mainPic
   # first check what @difficulty then select the two arrays and then 10 times randomly choose between each
   # run our answer checker program after each loop to track score.
-  def randomise
 
-<<<<<<< HEAD
-=======
-    10.times do {
->>>>>>> refs/remotes/origin/master
-       # set variable to see if it is trent to true or false.
-      if rand(1..100) > 50
-        @is_trent = true
+end
+
+
+def randomise
+
+   3.times do
+     # set variable to see if it is trent to true or false.
+    if rand(1..100) > 50
+      @is_trent = true
+    else
+      @is_trent = false
+    end
+
+     #now update which two sets of arrays to use.
+
+    case @difficulty
+    when "easy"
+      if @is_trent == true
+        mainPic = @easy_trent.sample
       else
-        @is_trent = false
+        mainPic = @easy_not_trent.sample
       end
 
-       #now update which two sets of arrays to use.
-<<<<<<< HEAD
-=======
-      case @difficulty
->>>>>>> refs/remotes/origin/master
-      when "easy"
-        if @is_trent == true
-          mainPic = @easy_trent.sample
-        else
-<<<<<<< HEAD
-          mainPic = @easy_not_trent.sample
-=======
->>>>>>> refs/remotes/origin/master
-        end
-
-      when "medium"
-        if @is_trent == true
-          mainPic = @medium_trent.sample
-        else
-<<<<<<< HEAD
-          mainPic = @medium_not_trent.sample
-=======
->>>>>>> refs/remotes/origin/master
-        end
-
-      when "hard"
-        if @is_trent == true
-          mainPic = @hard_trent.sample
-        else
-<<<<<<< HEAD
-          mainPic = @hard_not_trent.sample
-=======
->>>>>>> refs/remotes/origin/master
-        end
+    when "medium"
+      if @is_trent == true
+        mainPic = @medium_trent.sample
+      else
+        mainPic = @medium_not_trent.sample
       end
 
-      # this section prints to screen using our mainPic , chosen above
-      # and our amazing catpix gem... grr
-
-<<<<<<< HEAD
-=======
-      Catpix::print_image @mainPic,
->>>>>>> refs/remotes/origin/master
-        :limit_x => 0.7,
-        :limit_y => 0.7,
-        :center_x => true,
-        :center_y => true,
-        :bg => "white",
-        :bg_fill => true
-
-        print "Is it Trent?"
-
-        # answer checking program to track their score.
-<<<<<<< HEAD
-        #answer_checker
+    when "hard"
+      if @is_trent == true
+        mainPic = @hard_trent.sample
+      else
+        mainPic = @hard_not_trent.sample
+      end
     end
-  end
-=======
-        answer_checker
-    }
-    end
-  end
+
+    # this section prints to screen using our mainPic , chosen above
+    # and our amazing catpix gem... grr
+
+
+    Catpix::print_image mainPic,
+      :limit_x => 0.7,
+      :limit_y => 0.7,
+      :center_x => true,
+      :center_y => true,
+      :bg => "white",
+      :bg_fill => true
+
+
+      print "Is it Trent?"
+      answer = gets.chomp
+  #  end
+  end  # 10 times do end marker
 
 end
-
->>>>>>> refs/remotes/origin/master
-
-end
-
-
 # def answer_checker
 #   #
 #   puts "Is this Trent?"
