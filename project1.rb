26a88803615b57ec765d281e9694a45432c4060e
J1marotta <jim_is55@hotmail.com>
2017-08-10 18:11:21 +1000

    git problems

--- project1.rb
+++ project1.rb
@@ -25,16 +25,17 @@ class Introduction
 
   # main method for introduction and visual
   def begin
+    system("clear")
     a = Artii::Base.new(:font => 'slant')
     puts
     puts
-    puts a.asciify('             Welcome!')
+    puts a.asciify('                   Welcome!')
     puts
-    puts a.asciify('           Trent or not!')
+    puts a.asciify('                 Trent or not!')
     puts
-    puts "                       By\n\n"
-    puts "          James, Carmen, Nathan, and Olivia\n\n\n\n"
-    puts "            Hello , what is your  name?"
+    puts "                               By\n\n"
+    puts "                 James, Carmen, Nathan, and Olivia\n\n\n\n"
+    puts "                    Hello , what is your  name?"
 
     # accepts input and makes a new player class object with their name
     # and a default score of 0.
@@ -49,26 +50,24 @@ class Introduction
     loop do
     puts "Would you like to play on [easy], [medium], or [hard] mode?"
     @input = gets.chomp.downcase
-    @choice = difficulty
 
     # save their input as
     case @input
     when "easy", "e"
-        difficulty = easy
+        @difficulty = "easy"
         break
       when "medium", "m"
-        difficulty = medium
+        @difficulty = "medium"
       when "hard" , "h"
-        difficulty = hard
+        @difficulty = "hard"
       else
         "Error, invalid input, please choose Easy, Medium or Hard"
     end
 
+
   end
 
 end
-
-
 # class to handle our users, which tracks name and score. and can update and print their score.
 # consider adding terminal-table for printing the leaderboard once we have running
 class Player
@@ -78,16 +77,22 @@ class Player
     @score = score
   end
 
-  attr_accessor :name :score
+  attr_accessor :name, :score
 
   def tracker
       @score += 1
-      @list << user.score
+      @scoreBoard << user.score
   end
 
   def score_board
-    File.open("leaderboard", 'a+') {|f| f.write(@list) }
+    File.open("leaderboard", 'a+') {|f| f.write(@scoreBoard) }
   end
+
+  def wrong
+    puts "Wrong"
+
+  end
+end
 =begin
     w - Create an empty file for writing.
     a - Append to a file.The file is created if it does not exist.
@@ -96,9 +101,11 @@ class Player
     a+ - Open a file for reading and appending. The file is created if it does not exist.
 =end
 
-  end
+
 
 end
+
+
 
 # class to handle our images,
 class Image_keeper
@@ -108,60 +115,146 @@ class Image_keeper
 
   #Individual users will need to change the directory of where the images are stored
   def keep_files
-    @easy_trent = ["//Users/oliviamo/sites/project1/trenteasy1small.jpg",
-      "//Users/oliviamo/sites/project1/trenteasy2.jpg",
-      "//Users/oliviamo/sites/project1/trenteasy10.jpg"
+    @easy_trent = [
+      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy1small.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy2.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy3.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy4.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy5.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy6.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy7.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy8.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy9.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/easy/trenteasy10.jpg")
     ]
-    @easy_not_trent = []
-
-    @medium_trent = []
-    @medium_not_trent = []
-
-    @hard_trent = []
+    @easy_not_trent = [
+      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy1.jpg"),
+      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy2.jpg"),
+      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy3.jpg"),
+      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy4.jpg"),
+      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy5.jpg"),
+      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy6.jpg"),
+      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy7.jpg"),
+      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy8.jpg"),
+      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy9.jpg"),
+      open(ENV['HOME']+"/sites/project1/not_trent/easy/nteasy10.jpg")
+    ]
+    @medium_trent = [
+      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed1.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed2.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed3.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed4.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed5.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed6.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed7.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed8.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed9.jpg"),
+      open(ENV['HOME']+"/sites/project1/trent/medium/trentmed10.jpg")
+    ]
+    @medium_not_trent = [
+      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed1.jpg"),
+      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed2.jpg"),
+      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed3.jpg"),
+      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed4.jpg"),
+      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed5.jpg"),
+      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed6.jpg"),
+      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed7.jpg"),
+      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed8.jpg"),
+      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed9.jpg"),
+      open(ENV['HOME']+"sites/project1/not_trent/medium/ntmed10.jpg")
+    ]
+    @hard_trent = [
+      open(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
+      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
+      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
+      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
+      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
+      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
+      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
+      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
+      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
+      pen(ENV['HOME']+"sites/project1/trent/hard/trenthard1.jpg"),
+    ]
     @hard_not_trent = []
   end
 
   # method to randomly select the arrays of pictures.
   # variables :  picture they are looking at = mainPic
-  # first check what difficulty then select the two arrays and then 10 times randomly choose between each
+  # first check what @difficulty then select the two arrays and then 10 times randomly choose between each
   # run our answer checker program after each loop to track score.
   def randomise
 
+    10.times do
+       # set variable to see if it is trent to true or false.
+      if rand(1..100) > 50
+        @is_trent = true
+      else
+        @is_trent = false
+      end
 
+       #now update which two sets of arrays to use.
+      case @difficulty
+      when "easy"
+        if @is_trent == true
+          mainPic = @easy_trent.sample
+        else
+          mainPic = @easy_not_trent.sample
+        end
 
+      when "medium"
+        if @is_trent == true
+          mainPic = @medium_trent.sample
+        else
+          mainPic = @medium_not_trent.sample
+        end
 
+      when "hard"
+        if @is_trent == true
+          mainPic = @hard_trent.sample
+        else
+          mainPic = @hard_not_trent.sample
+        end
+      end
 
-    Catpix::print_image @mainPic,
-      :limit_x => 0.7,
-      :limit_y => 0.7,
-      :center_x => true,
-      :center_y => true,
-      :bg => "white",
-      :bg_fill => true
+      # this section prints to screen using our mainPic , chosen above
+      # and our amazing catpix gem... grr
 
-      print "Is it Trent?"
+      Catpix::print_image mainpic,
+        :limit_x => 0.7,
+        :limit_y => 0.7,
+        :center_x => true,
+        :center_y => true,
+        :bg => "white",
+        :bg_fill => true
 
-      # answer checking program to track their score.
-      answer_checker
+        print "Is it Trent?"
+
+        # answer checking program to track their score.
+        #answer_checker
     end
   end
 
 end
 
+
+# def answer_checker
+#   #
+#   puts "Is this Trent?"
+#   isTrentAnswer = gets.chomp.downcase
+#
+#   # case answer with
+#   case isTrentAnswer
+#   when  "yes" && @is_trent?
+#     user.tracker
+#   when  "no" && !@is_trent?
+#     user.tracker
+#   when "no" && @is_trent?
+#     user.wrong
+#   when "yes" && !@is_trent?
+#     user.wrong
+#   end
+
+
 intro = Introduction.new
 intro.begin
 intro.mode
-
-def answer_checker
-  puts "Is this Trent or not? [y/n]"
-  isTrentAnswer = gets.chomp.downcase
-  case isTrentAnswer
-  when  "yes" && @is_trent?
-    user.tracker
-  when  "no" && !@is_trent?
-    user.tracker
-  when "no" && @is_trent?
-    user.wrong
-  when "yes" && !@is_trent?
-    user.wrong
-  end
